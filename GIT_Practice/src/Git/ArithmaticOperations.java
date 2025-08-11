208c296e672c56575cf121560d38809042c46622
omdha_gh7d4bj <omdha_gh7d4bj@192.168.1.16>
2025-08-11 16:36:21 +0530

    Addition Logic Developed By Dev1

---
+++ GIT_Practice/src/Git/ArithmaticOperations.java
@@ -0,0 +1,14 @@
+package Git;
+
+import java.util.Scanner;
+
+public class ArithmaticOperations {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter 1st Number:");
+		int x = sc.nextInt();
+		System.out.print("Enter 1st Number:");
+		int y = sc.nextInt();
+		System.out.println("Adddition is: "+(x+y));
+	}
+}
