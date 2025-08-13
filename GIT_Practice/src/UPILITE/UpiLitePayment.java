a4e408c99c08c06873098ede274a073ef66db7df
omdhanve <omdhanve30@gmail.com>
2025-08-13 16:38:44 +0530

    UPILITE added

---
+++ GIT_Practice/src/UPILITE/UpiLitePayment.java
@@ -0,0 +1,7 @@
+package UPILITE;
+
+public class UpiLitePayment {
+  publoc String doPaymentUsingPhonePeLite(Double amt, long phno) {
+	  return "Rs"+amt+" Payment is Done using PhonePeLite to no:"+phno;
+  }
+}
