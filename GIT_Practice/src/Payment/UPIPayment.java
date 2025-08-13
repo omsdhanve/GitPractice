14f3eac6808eab8a3d0b1089d2679f80727ca60b
omdha_gh7d4bj <omdha_gh7d4bj@192.168.1.16>
2025-08-13 15:39:16 +0530

    Dev2 UPIPayment

--- GIT_Practice/src/Payment/UPIPayment.java
+++ GIT_Practice/src/Payment/UPIPayment.java
@@ -2,6 +2,11 @@ package Payment;
 
 public class UPIPayment {
 	public String doPaymentUsingPhonePe(long phno,double amount) {
-		return "Rs."+amount+" is sending to phno: "+phno;
+		return "Rs."+amount+" is sending to phno: "+phno+" using phonepe";
 	}
+	
+	public String doPaymentUsingGPay(long phno,double amount) {
+		return "Rs."+amount+" is sending to phno: "+phno+" using GPay";
+	}
+	
 }
