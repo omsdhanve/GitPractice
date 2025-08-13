14a3235432910ce291fd510811e3c8994523ac67
omdhanve <omdhanve30@gmail.com>
2025-08-13 16:41:41 +0530

    amazonpelite addeed b=y dev2

--- GIT_Practice/src/UPILITE/UpiLitePayment.java
+++ GIT_Practice/src/UPILITE/UpiLitePayment.java
@@ -1,7 +1,11 @@
 package UPILITE;
 
 public class UpiLitePayment {
-  publoc String doPaymentUsingPhonePeLite(Double amt, long phno) {
+  public String doPaymentUsingPhonePeLite(Double amt, long phno) {
 	  return "Rs"+amt+" Payment is Done using PhonePeLite to no:"+phno;
+  }
+  
+  public String doPaymentUsingAmazonPeLite(Double amt, long phno) {
+	  return "Rs"+amt+" Payment is Done using AmazonPeLite to no:"+phno;
   }
 }
