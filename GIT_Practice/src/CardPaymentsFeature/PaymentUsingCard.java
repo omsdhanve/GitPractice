18b5f25c483b9f629adcb016f2a901b4e9840ab1
omdhanve <omdhanve30@gmail.com>
2025-08-13 16:05:22 +0530

    Dev2 CreditCard Payment

--- GIT_Practice/src/CardPaymentsFeature/PaymentUsingCard.java
+++ GIT_Practice/src/CardPaymentsFeature/PaymentUsingCard.java
@@ -4,4 +4,8 @@ public class PaymentUsingCard {
 	public String PayUsingDebitCard(long cardNo, Double amt) {
 		return "Rs."+amt+" bill payment done using DebitCard";
 	}
+	
+	public String PayUsingCreditCard(long cardNo, Double amt) {
+		return "Rs."+amt+" bill payment done using CreditCard";
+	}
 }
